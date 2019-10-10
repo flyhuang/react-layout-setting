26ece4fa21ebbce263292a5a007b771f3cf81c73
Wade Huang <47033631@qq.com>
2019-10-10 11:43:43 +0800

    test(story): remove css inject.

--- packages/react-layout-setting/stories/index.stories.js
+++ packages/react-layout-setting/stories/index.stories.js
@@ -4,7 +4,6 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
 import ReactLayoutSetting from '../src/index.js';
-import '../src/index.css'
 
 storiesOf('React Layout Setting', module)
     .add('Hover to active td', () => <ReactLayoutSetting callback={(row, col) => {
